054f85101b9eb132571e3d919bc450dc20217a19
Samuel Walz <mail@samwalz.com>
2018-05-28 10:25:28 +0200

    added initial version of ZummaPC;

---
+++ driver/ZummaPC.js
@@ -0,0 +1,93 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define(["require", "exports", "system_lib/Driver", "system_lib/Metadata"], function (require, exports, Driver_1, Meta) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var ZummaPC = (function (_super) {
+        __extends(ZummaPC, _super);
+        function ZummaPC(socket) {
+            var _this = _super.call(this, socket) || this;
+            _this.socket = socket;
+            socket.enableWakeOnLAN();
+            return _this;
+        }
+        ZummaPC.prototype.tell = function (data) {
+            this.socket.sendText(data);
+        };
+        Object.defineProperty(ZummaPC.prototype, "power", {
+            get: function () {
+                return this.powerState;
+            },
+            set: function (on) {
+                if (this.powerState != on) {
+                    this.powerState = on;
+                    if (on)
+                        this.powerUp2();
+                    else
+                        this.powerDown();
+                }
+            },
+            enumerable: true,
+            configurable: true
+        });
+        ZummaPC.prototype.powerUp = function () {
+            if (!this.powerState) {
+                this.powerState = true;
+                this.changed('power');
+            }
+            return this.powerUp2();
+        };
+        ZummaPC.prototype.powerUp2 = function () {
+            var _this = this;
+            if (!this.poweringUp) {
+                this.socket.wakeOnLAN();
+                this.poweringUp = new Promise(function (resolver, rejector) {
+                    _this.powerUpResolver = resolver;
+                    wait(40000).then(function () {
+                        rejector("Timeout");
+                        delete _this.poweringUp;
+                        delete _this.powerUpResolver;
+                    });
+                });
+            }
+            return this.poweringUp;
+        };
+        ZummaPC.prototype.powerDown = function () {
+            this.tell("stop");
+        };
+        __decorate([
+            Meta.property("Power on/off"),
+            __metadata("design:type", Boolean),
+            __metadata("design:paramtypes", [Boolean])
+        ], ZummaPC.prototype, "power", null);
+        __decorate([
+            Meta.callable("Power up using wake-on-LAN"),
+            __metadata("design:type", Function),
+            __metadata("design:paramtypes", []),
+            __metadata("design:returntype", Promise)
+        ], ZummaPC.prototype, "powerUp", null);
+        ZummaPC = __decorate([
+            Meta.driver('NetworkUDP', { port: 32400 }),
+            __metadata("design:paramtypes", [Object])
+        ], ZummaPC);
+        return ZummaPC;
+    }(Driver_1.Driver));
+    exports.ZummaPC = ZummaPC;
+});
---
+++ driver/ZummaPC.ts
@@ -0,0 +1,110 @@
+/*
+ * Created 2017 by Mike Fahl.
+ */
+
+import {NetworkUDP} from "system/Network";
+import {Driver} from "system_lib/Driver";
+import * as Meta from "system_lib/Metadata";
+
+/**
+ A driver using a UDP socket for communicating with a "device" running Zumma.
+
+ The port value specified indicates the default UDP port number,
+ selected automatically when chosing this driver. The 'NetworkUDP' string
+ specifies that this driver is intended for that type of subsystem, and
+ its constructor will accept that type.
+ */
+@Meta.driver('NetworkUDP', { port: 32400 })
+export class ZummaPC extends Driver<NetworkUDP> {
+	// IMPORTANT: The class name above MUST match the name of the
+	// file (minus its extension).
+
+    private powerState: boolean;
+
+	private poweringUp: Promise<void>;	// Set while waiting to be powered up
+    private powerUpResolver: (value?: any) => void;
+
+	/**
+	 * Create me, attached to the network socket I communicate through. When using a
+	 * driver, the driver replaces the built-in functionality of the network socket
+	 with the properties and callable functions exposed.
+	 */
+	public constructor(private socket: NetworkUDP) {
+		super(socket);
+        socket.enableWakeOnLAN();
+	}
+
+
+	/**
+	 * Tell Zumma something through the UDP socket. Funnel most commands through here
+	 * to also allow them to be logged, which makes testing easier.
+	 */
+	private tell(data: string) {
+		// console.info('tell', data);
+		this.socket.sendText(data);
+	}
+
+
+    /**
+     Turn power on/off. Turning off is instant. Turning on takes depending on PC,
+     and is implemented through WoL
+     As an alternative to setting power to true, call powerUp()
+     and await its returned promise.
+     */
+    @Meta.property("Power on/off")
+    public set power(on: boolean) {
+        if (this.powerState != on) {
+            this.powerState = on;
+            if (on)
+                this.powerUp2();	// This takes a while
+            else 	// Tell display to power down
+                this.powerDown();
+        }
+        // console.log("Sent", cmd);
+    }
+    /**
+     Get current power state, if known, else undefined.
+     */
+    public get power(): boolean {
+        return this.powerState;
+    }
+
+    /**
+     * Power up using wake-on-LAN. Returned promise resolved
+     * once connected.
+     */
+    @Meta.callable("Power up using wake-on-LAN")
+    public powerUp(): Promise<void> {
+        if (!this.powerState) {
+            this.powerState = true;		// Indicates desired state
+            this.changed('power');
+        }
+        return this.powerUp2();
+    }
+
+    /**
+     * Send wake-on-LAN, unless one is already in progress.
+     * Return promise resolved once powered up.
+     */
+    private powerUp2(): Promise<void> {
+        if (!this.poweringUp) {
+            this.socket.wakeOnLAN();
+            this.poweringUp = new Promise<void>((resolver, rejector)=> {
+                this.powerUpResolver = resolver;
+                wait(40000).then(()=> {
+                    rejector("Timeout");
+                    delete this.poweringUp;
+                    delete this.powerUpResolver;
+                });
+            });
+        }
+        return this.poweringUp;
+    }
+
+    // Send command to turn power off
+    private powerDown() {
+        this.tell("stop");
+    }
+
+
+}
