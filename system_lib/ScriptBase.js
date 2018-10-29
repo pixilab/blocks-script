1a27c5799d1a9775c374f4b4f04ae4abf11cee5d
Mike Fahl <mike@pixilab.se>
2018-10-29 15:24:32 +0100

    Added Script getProperty along with PreopertyAccessor object.

--- system_lib/ScriptBase.js
+++ system_lib/ScriptBase.js
@@ -1,10 +1,19 @@
+/*
+ * Copyright (c) 2018 PIXILAB Technologies AB, Sweden (http://pixilab.se). All Rights Reserved.
+ */
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    /**
+     * Common stuff shared between user scripts and drivers.
+     */
     var ScriptBase = (function () {
         function ScriptBase(scriptFacade) {
             this.__scriptFacade = scriptFacade;
         }
+        /**	Inform others that prop has changed, causing any
+         *	subscribers to be notified soon.
+         */
         ScriptBase.prototype.changed = function (prop) {
             this.__scriptFacade.changed(prop);
         };
