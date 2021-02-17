c9c9054334e70b2033fe9354243b2299e7372301
Samuel Walz <sw@noparking.dk>
2021-02-17 09:48:13 +0100

    fixed driver

--- driver/AnthemMRX_x20.js
+++ driver/AnthemMRX_x20.js
@@ -2,7 +2,7 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
         return extendStatics(d, b);
     };
     return function (d, b) {
@@ -20,22 +20,128 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 define(["require", "exports", "system_lib/Metadata", "driver/NetworkProjector"], function (require, exports, Metadata_1, NetworkProjector_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    exports.AnthemMRX_x20 = void 0;
+    var ZONE_ALL = 'Z0';
+    var ZONE_MAIN = 'Z1';
+    var CMD_FPB = 'FPB';
+    var CMD_SIP = 'SIP';
+    var CMD_MUT = 'MUT';
+    var CMD_POW = 'POW';
+    var CMD_VOL = 'VOL';
+    var CMD_IDQ = 'IDQ';
+    var CMD_IDM = 'IDM';
+    var CMD_IDS = 'IDS';
+    var CMD_IDR = 'IDR';
+    var CMD_IDB = 'IDB';
+    var CMD_IDH = 'IDH';
+    var CMD_IDN = 'IDN';
+    var CMD_MSG = 'MSG';
+    var ERROR_CANNOT_BE_EXECUTED_PREFIX = '!E';
+    var ERROR_OUT_OF_RANGE_PREFIX = '!R';
+    var ERROR_INVALID_COMMAND_PREFIX = '!I';
+    var ERROR_ZONE_OFF_PREFIX = '!Z';
     var AnthemMRX_x20 = (function (_super) {
         __extends(AnthemMRX_x20, _super);
         function AnthemMRX_x20(socket) {
             var _this = this;
             socket.setReceiveFraming(';');
             _this = _super.call(this, socket) || this;
-            _this.addState(_this._power = new NetworkProjector_1.BoolState('Z0POW', 'power'));
+            _this.addState(_this._power = new NetworkProjector_1.BoolState(ZONE_MAIN + CMD_POW, 'power'));
+            _this.addState(_this._powerAll = new NetworkProjector_1.BoolState(ZONE_ALL + CMD_POW, 'powerAll'));
+            _this.addState(_this._fpb = new NetworkProjector_1.NumState(CMD_FPB, 'frontPanelBrightness', 0, 3));
+            _this.addState(_this._mut = new NetworkProjector_1.BoolState(ZONE_MAIN + CMD_MUT, 'mute'));
+            _this.addState(_this._vol = new SignedNumberState(ZONE_MAIN + CMD_VOL, 'volume'));
+            _this.poll();
             _this.attemptConnect();
             console.log(_this.socket.fullName);
             return _this;
         }
+        Object.defineProperty(AnthemMRX_x20.prototype, "frontPanelBrightness", {
+            get: function () {
+                return this._fpb.get();
+            },
+            set: function (value) {
+                if (this._fpb.set(Math.round(value))) {
+                    this.sendCorrection();
+                }
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(AnthemMRX_x20.prototype, "mute", {
+            get: function () {
+                return this._mut.get();
+            },
+            set: function (value) {
+                if (this._mut.set(value)) {
+                    this.sendCorrection();
+                }
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(AnthemMRX_x20.prototype, "powerAll", {
+            get: function () {
+                return this._power.get();
+            },
+            set: function (on) {
+                if (this._powerAll.set(on))
+                    this.sendCorrection();
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(AnthemMRX_x20.prototype, "volume", {
+            get: function () {
+                return this._vol.get();
+            },
+            set: function (value) {
+                if (this._vol.set(value)) {
+                    this.sendCorrection();
+                }
+            },
+            enumerable: false,
+            configurable: true
+        });
+        AnthemMRX_x20.prototype.displayMessage = function (message, row) {
+            this.sendText(ZONE_MAIN + CMD_MSG +
+                Math.round(row) +
+                message.substr(0, 100));
+        };
         AnthemMRX_x20.prototype.textReceived = function (text) {
-            console.log(text);
+            var result = text;
+            if (text[0] == '!') {
+                var firstTwoChars = text.substr(0, 2);
+                var followingChars = text.substr(2);
+                var failed = false;
+                switch (firstTwoChars) {
+                    case ERROR_CANNOT_BE_EXECUTED_PREFIX:
+                        console.warn('can not be executed: "' + followingChars + '"');
+                        break;
+                    case ERROR_OUT_OF_RANGE_PREFIX:
+                        console.warn('command out of range: "' + followingChars + '"');
+                        break;
+                    case ERROR_INVALID_COMMAND_PREFIX:
+                        console.error('invalid command: "' + followingChars + '"');
+                        break;
+                    case ERROR_ZONE_OFF_PREFIX:
+                        console.warn('zone is off: "' + followingChars + '"');
+                        failed = true;
+                        break;
+                }
+                if (failed) {
+                    this.requestFailure(text);
+                    return;
+                }
+            }
+            this.requestSuccess(result);
+            this.requestFinished();
         };
         AnthemMRX_x20.prototype.justConnected = function () {
             console.log('connected');
@@ -47,10 +153,13 @@ define(["require", "exports", "system_lib/Metadata", "driver/NetworkProjector"],
         AnthemMRX_x20.prototype.pollStatus = function () {
             var _this = this;
             if (this.okToSendCommand()) {
-                this.request('Z0POW').then(function (reply) {
-                    var on = (parseInt(reply) & 1) != 0;
-                    if (!_this.inCmdHoldoff())
-                        _this._power.updateCurrent(on);
+                var powerRequest_1 = ZONE_MAIN + CMD_POW;
+                this.request(powerRequest_1).then(function (reply) {
+                    if (reply.substr(0, powerRequest_1.length) == powerRequest_1) {
+                        var on = (parseInt(reply.substr(powerRequest_1.length)) & 1) != 0;
+                        if (!_this.inCmdHoldoff())
+                            _this._power.updateCurrent(on);
+                    }
                 }).catch(function (error) {
                     _this.warnMsg("pollStatus error", error);
                     _this.disconnectAndTryAgainSoon();
@@ -62,8 +171,7 @@ define(["require", "exports", "system_lib/Metadata", "driver/NetworkProjector"],
             var _this = this;
             var toSend = question;
             toSend += (param === undefined) ? '?' : param;
-            console.info("request", toSend);
-            this.socket.sendText(toSend).catch(function (err) { return _this.sendFailed(err); });
+            this.socket.sendText(toSend, this.getDefaultEoln()).catch(function (err) { return _this.sendFailed(err); });
             var result = this.startRequest(toSend);
             result.finally(function () {
                 asap(function () {
@@ -96,6 +204,38 @@ define(["require", "exports", "system_lib/Metadata", "driver/NetworkProjector"],
         AnthemMRX_x20.prototype.okToSendCommand = function () {
             return !this.busyHoldoff && _super.prototype.okToSendCommand.call(this);
         };
+        __decorate([
+            Metadata_1.property('Front panel brightness: 0=off, 1=low, 2=medium, 3=high'),
+            Metadata_1.min(0),
+            Metadata_1.max(3),
+            __metadata("design:type", Number),
+            __metadata("design:paramtypes", [Number])
+        ], AnthemMRX_x20.prototype, "frontPanelBrightness", null);
+        __decorate([
+            Metadata_1.property("Mute"),
+            __metadata("design:type", Boolean),
+            __metadata("design:paramtypes", [Boolean])
+        ], AnthemMRX_x20.prototype, "mute", null);
+        __decorate([
+            Metadata_1.property('Power All Zones on/off'),
+            __metadata("design:type", Boolean),
+            __metadata("design:paramtypes", [Boolean])
+        ], AnthemMRX_x20.prototype, "powerAll", null);
+        __decorate([
+            Metadata_1.property("Volume, Main Zone"),
+            Metadata_1.min(-90),
+            Metadata_1.max(10),
+            __metadata("design:type", Number),
+            __metadata("design:paramtypes", [Number])
+        ], AnthemMRX_x20.prototype, "volume", null);
+        __decorate([
+            Metadata_1.callable('Display Message'),
+            __param(0, Metadata_1.parameter('message')),
+            __param(1, Metadata_1.parameter('row 0-1', true)),
+            __metadata("design:type", Function),
+            __metadata("design:paramtypes", [String, Number]),
+            __metadata("design:returntype", void 0)
+        ], AnthemMRX_x20.prototype, "displayMessage", null);
         AnthemMRX_x20 = __decorate([
             Metadata_1.driver('NetworkTCP', { port: 14999 }),
             __metadata("design:paramtypes", [Object])
@@ -103,4 +243,27 @@ define(["require", "exports", "system_lib/Metadata", "driver/NetworkProjector"],
         return AnthemMRX_x20;
     }(NetworkProjector_1.NetworkProjector));
     exports.AnthemMRX_x20 = AnthemMRX_x20;
+    var StringState = (function (_super) {
+        __extends(StringState, _super);
+        function StringState() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        StringState.prototype.correct = function (drvr) {
+            return this.correct2(drvr, this.wanted);
+        };
+        return StringState;
+    }(NetworkProjector_1.State));
+    var SignedNumberState = (function (_super) {
+        __extends(SignedNumberState, _super);
+        function SignedNumberState() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        SignedNumberState.prototype.correct = function (drvr) {
+            return this.correct2(drvr, (this.wanted >= 0 ? '+' : '') + this.wanted.toString());
+        };
+        SignedNumberState.prototype.set = function (v) {
+            return _super.prototype.set.call(this, Math.round(v));
+        };
+        return SignedNumberState;
+    }(NetworkProjector_1.State));
 });
--- driver/AnthemMRX_x20.ts
+++ driver/AnthemMRX_x20.ts
@@ -6,32 +6,128 @@
 
  	Copyright (c) 2020 No Parking Production ApS, Denmark (https://noparking.dk). All Rights Reserved.
 	Created by: Samuel Walz <mail@samwalz.com>
-    Version: 0.1
+    Version: 0.3
     Features:
-    - power on/off
-
+    - power on/off (main / all)
+	- volume / mute (main)
+	- misc (front screen brightness, display message)
 
  */
 
 import {NetworkTCP} from "system/Network";
-import {callable, driver, parameter, property} from "system_lib/Metadata";
-import {BoolState, NetworkProjector, NumState} from "driver/NetworkProjector";
+import {callable, driver, max, min, parameter, property} from "system_lib/Metadata";
+import {BoolState, NetworkProjector, NumState, State} from "driver/NetworkProjector";
 import {Driver} from "system_lib/Driver";
+
+const ZONE_ALL = 'Z0';
+const ZONE_MAIN = 'Z1';
+
+
+/* read / write */
+/* global */
+/** Front panel brightness: 0=off, 1=low, 2=medium, 3=high */ const CMD_FPB = 'FPB';
+/** Standby IP Control: 0=disabled, 1=enabled */ const CMD_SIP = 'SIP';
+/* zone specific (need zone prefix Zx) */
+const CMD_MUT = 'MUT'; // Mute: 0=unmute, 1=mute
+const CMD_POW = 'POW'; // power
+const CMD_VOL = 'VOL'; // Volume setting: s=sign: +/-, yy=value.  Example: Z1VOL-35 represents main zone volume set to -35 dB.  Entry is rounded to nearest valid value.
+/* read only */
+/** Query model and firmware version */ const CMD_IDQ = 'IDQ';
+/** Query model */                      const CMD_IDM = 'IDM';
+/** Query software version */           const CMD_IDS = 'IDS';
+/** Query region */                     const CMD_IDR = 'IDR';
+/** Query software build date */        const CMD_IDB = 'IDB';
+/** Query hardware version */           const CMD_IDH = 'IDH';
+/** Query MCU MAC Address */            const CMD_IDN = 'IDN';
+/* write only */
+/** Z1MSGxyyyy x=row 0-1, yyyy=message */ const CMD_MSG = 'MSG';
+
+const ERROR_CANNOT_BE_EXECUTED_PREFIX = '!E';
+const ERROR_OUT_OF_RANGE_PREFIX = '!R';
+const ERROR_INVALID_COMMAND_PREFIX = '!I';
+const ERROR_ZONE_OFF_PREFIX = '!Z';
 
 
 @driver('NetworkTCP', { port: 14999 })
 export class AnthemMRX_x20 extends NetworkProjector {
-    private busyHoldoff?: CancelablePromise<void>;	// See projectorBusy()
+
+	private busyHoldoff?: CancelablePromise<void>;	// See projectorBusy()
 	private recentCmdHoldoff?: CancelablePromise<void>;	// See sentCommand()
+
+	private _fpb: NumState;
+	private _mut: BoolState;
+	private _powerAll: BoolState;
+	private _sip: BoolState;
+	private _vol: SignedNumberState;
+
     constructor(socket: NetworkTCP) {
         socket.setReceiveFraming(';');
 		super(socket);
-        this.addState(this._power = new BoolState('Z0POW', 'power'));
-        // this.poll();	// Get polling going
+        this.addState(this._power = new BoolState(ZONE_MAIN + CMD_POW, 'power'));
+		this.addState(this._powerAll = new BoolState(ZONE_ALL + CMD_POW, 'powerAll'));
+		this.addState(this._fpb = new NumState(CMD_FPB, 'frontPanelBrightness', 0, 3));
+		this.addState(this._mut = new BoolState(ZONE_MAIN + CMD_MUT, 'mute'));
+		this.addState(this._vol = new SignedNumberState(ZONE_MAIN + CMD_VOL, 'volume'));
+        this.poll();	// Get polling going
 		this.attemptConnect();	// Attempt initial connection
         console.log(this.socket.fullName);
         // socket.enableWakeOnLAN();
-        // console.info("inited");
+
+	}
+
+	@property('Front panel brightness: 0=off, 1=low, 2=medium, 3=high')
+	@min(0)
+	@max(3)
+	public set frontPanelBrightness(value: number) {
+		if (this._fpb.set(Math.round(value))) {
+            this.sendCorrection();
+        }
+	}
+	public get frontPanelBrightness(): number {
+		return this._fpb.get();
+	}
+
+	@property("Mute")
+	public set mute(value: boolean) {
+		if (this._mut.set(value)) {
+            this.sendCorrection();
+        }
+	}
+	public get mute(): boolean {
+		return this._mut.get();
+	}
+
+	@property('Power All Zones on/off')
+	public set powerAll(on: boolean) {
+		if (this._powerAll.set(on))
+			this.sendCorrection();
+	}
+	public get powerAll(): boolean {
+		return this._power.get();
+	}
+
+	@property("Volume, Main Zone")
+	@min(-90)
+	@max(10)
+	public set volume(value: number) {
+		if (this._vol.set(value)) {
+            this.sendCorrection();
+        }
+	}
+	public get volume(): number {
+		return this._vol.get();
+	}
+
+	@callable('Display Message')
+	public displayMessage(
+		@parameter('message') message: string,
+		@parameter('row 0-1', true) row?: number
+	): void {
+		this.sendText(
+			ZONE_MAIN + CMD_MSG +
+			Math.round(row) +
+			message.substr(0, 100)
+		);
 	}
 
     // @callable('wake device')
@@ -40,7 +136,33 @@ export class AnthemMRX_x20 extends NetworkProjector {
     // }
 
     protected textReceived(text: string): void {
-        console.log(text);
+		let result = text;
+		if (text[0] == '!') {
+			const firstTwoChars = text.substr(0, 2);
+			const followingChars = text.substr(2);
+			let failed = false;
+			switch (firstTwoChars) {
+				case ERROR_CANNOT_BE_EXECUTED_PREFIX:
+					console.warn('can not be executed: "' + followingChars + '"');
+					break;
+				case ERROR_OUT_OF_RANGE_PREFIX:
+					console.warn('command out of range: "' + followingChars + '"');
+					break;
+				case ERROR_INVALID_COMMAND_PREFIX:
+					console.error('invalid command: "' + followingChars + '"');
+					break;
+				case ERROR_ZONE_OFF_PREFIX:
+					console.warn('zone is off: "' + followingChars + '"');
+					failed = true;
+					break;
+			}
+			if (failed) {
+				this.requestFailure(text);
+				return;
+			}
+		}
+		this.requestSuccess(result);
+		this.requestFinished();
     }
     protected justConnected(): void {
         console.log('connected');
@@ -51,13 +173,14 @@ export class AnthemMRX_x20 extends NetworkProjector {
 	}
     protected pollStatus(): boolean {
 		if (this.okToSendCommand()) {	// Don't interfere with command already in flight
-			this.request('Z0POW').then(
+			const powerRequest = ZONE_MAIN + CMD_POW;
+			this.request(powerRequest).then(
 				reply => {
-					const on = (parseInt(reply) & 1) != 0;
-					if (!this.inCmdHoldoff())
-						this._power.updateCurrent(on);
-					// if (on && this.okToSendCommand()) // Attempt input too on case never done
-					// 	this.getInputState(true);
+					if (reply.substr(0, powerRequest.length) == powerRequest) {
+						const on = (parseInt(reply.substr(powerRequest.length)) & 1) != 0;
+						if (!this.inCmdHoldoff())
+							this._power.updateCurrent(on);
+					}
 				}
 			).catch(error => {
 				this.warnMsg("pollStatus error", error);
@@ -69,11 +192,9 @@ export class AnthemMRX_x20 extends NetworkProjector {
 	}
     request(question: string, param?: string): Promise<string> {
         var toSend = question;
-        // toSend += ' ';
         toSend += (param === undefined) ? '?' : param;
-        // toSend += ';';
-        console.info("request", toSend);
-        this.socket.sendText(toSend).catch(
+        // console.info("request", toSend);
+        this.socket.sendText(toSend, this.getDefaultEoln()).catch(
             err=>this.sendFailed(err)
         );
         const result = this.startRequest(toSend);
@@ -108,4 +229,30 @@ export class AnthemMRX_x20 extends NetworkProjector {
 		return !this.busyHoldoff && super.okToSendCommand();
 	}
 
+}
+
+class StringState extends State<string> {
+	correct(drvr: NetworkProjector): Promise<string> {
+		return this.correct2(drvr, this.wanted);
+	}
+}
+class SignedNumberState extends State<number> {
+	correct(drvr: NetworkProjector): Promise<string> {
+		return this.correct2(drvr, (this.wanted >= 0 ? '+' : '') + this.wanted.toString());
+	}
+
+	set(v: number): boolean {
+		return super.set(Math.round(v));
+	}
+
+
+	// get(): number {
+	// 	var result = super.get();
+	// 	// Better return min/0 than undefined or some invalid type of data
+	// 	if (typeof result !== 'number') {
+	// 		console.error("Value invalid for", this.baseCmd, result);
+	// 		result = this.min || 0;
+	// 	}
+	// 	return result;
+	// }
 }
